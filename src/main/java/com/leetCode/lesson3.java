3ed670d0cb23b4d281545efacaccdd373f39eb13
金钊 <jinzhao@csii.com.cn>
2021-03-04 16:35:08 +0800

    罗马数字转整数

---
+++ src/main/java/com/leetCode/lesson3.java
@@ -0,0 +1,35 @@
+package com.leetCode;
+
+public class lesson3 {
+    /***
+     * 回文数
+     *
+     * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+     *
+     * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/palindrome-number
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int help = 1;
+        int tmp = x;
+        while (tmp >= 10) {
+            help *= 10;
+            tmp /= 10;
+        }
+        while (x != 0) {
+            if (x % 10 != x / help) {
+                return false;
+            }
+            x = x % help / 10;
+            help /= 100;
+        }
+        return true;
+    }
+}
